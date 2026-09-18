f1c3ae437d41066acb91729fd8a3bd53711e86ce
Nandani Garg <gargnandani2004@gmail.com>
2026-09-18 13:48:58 +0530

    fix cloudinary env variable name

--- init/index.js
+++ init/index.js
@@ -1,24 +1,24 @@
+
+
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") }); // Load .env from root
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 const User = require("../models/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 async function initDB() {
   await mongoose.connect(MONGO_URL);
   console.log("connected to DB");
 
-  const ownerUsername = process.env.OWNER_USERNAME;
-  if (!ownerUsername) {
-    throw new Error(
-      "Set OWNER_USERNAME to the username that should own the sample listings."
-    );
-  }
-
+  const ownerUsername = process.env.OWNER_USERNAME || "admin"; // Fallback username agar env na mile
+  
   const owner = await User.findOne({ username: ownerUsername });
   if (!owner) {
-    throw new Error(`User "${ownerUsername}" was not found.`);
+    throw new Error(`User "${ownerUsername}" was not found in DB.`);
   }
 
   await Listing.deleteMany({});
